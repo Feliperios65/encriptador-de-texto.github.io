1bfe25ce801fd58487bb98b593a74622d9d09f8b
Felipe Rios <feliperios665@gmail.com>
2023-04-24 14:50:45 -0500

    mejorando codigo

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const textarea = document.querySelector("textarea");//seleccionando el DOOM
-const btn_encriptar = document.querySelector(".btn_encriptar");
+const btn_encriptar = document.querySelector(".btn_encriptar");//seleccionando el DOOM
 const btn_desencriptar = document.querySelector(".btn_desencriptar");
 const txt_encriptado = document.querySelector(".encriptado")
 
 
 btn_encriptar.addEventListener('click', function(){// se le asigna el vento click al boton para que itere el array
     txt_encriptado.classList.add('txt_encriptado')
-    let mensaje = textarea.value.split("")// comvierte lo que hay en el textarea en array
-    for (let i = 0; i < mensaje.length; i++) {  
+    const textarea = document.querySelector("textarea").value.split("");// comvierte lo que hay en el textarea en array
+    for (let i = 0; i < textarea.length; i++) {  
            encriptar(i);
     }
-    txt_encriptado.textContent = mensaje.join("");
+    txt_encriptado.textContent = textarea.join("");
 
     function encriptar(i){    //funcion para encriptar
-    switch (mensaje[i]) {
+    switch (textarea[i]) {
         case 'a':
-            mensaje[i] = "ai";
+            textarea[i] = "ai";
             break;
         case 'e':
-            mensaje[i] = "enter";
+            textarea[i] = "enter";
             break;  
         case 'i':
-            mensaje[i] = "imes";
+            textarea[i] = "imes";
             break; 
         case 'o':
-            mensaje[i] = "ober";
+            textarea[i] = "ober";
             break;
         case 'u':
-            mensaje[i] = "ufat";  
+            textarea[i] = "ufat";  
             break;
         default:
             break;
