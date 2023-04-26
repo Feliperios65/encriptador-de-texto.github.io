bd68ca74049653b3f0e70162be93f5dfe57be253
Felipe Rios <feliperios665@gmail.com>
2023-04-25 21:41:56 -0500

    funciones para botones: copiar, desencriptar y encriptar creadas y funcionando

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const btn_encriptar = document.querySelector(".btn_encriptar");
 const btn_desencriptar = document.querySelector(".btn_desencriptar");               //seleccionando el DOOM
-const txt_encriptado = document.querySelector(".encriptado")
+const txt_encriptado = document.querySelector(".encriptado");
+const boton_copiar = document.getElementById('boton');
+
 
 
 btn_encriptar.addEventListener('click', function(){// se le asigna el vento click al boton
     const textarea = document.querySelector("textarea").value;//traemos el value del textarea
     txt_encriptado.classList.add('txt_encriptado');//clase para dar estilo donde aparece el texto encriptado
-    txt_encriptado.textContent = encriptar();//imprimir en txt_encriptado el retorno de la funcion llamada
-    //funcion para encriptar
-    function encriptar(){
-        let rpl_1 = textarea.replace(/e/g, "enter")
-        let rpl_2 = rpl_1.replace(/i/g, "imes")
-        let rpl_3 = rpl_2.replace(/a/g, "ai")        //remplazar letra por letra el texto que devuelve cada replace
-        let rpl_4 = rpl_3.replace(/o/g, "ober")
-        let rpl_5 = rpl_4.replace(/u/g, "ufat")
-        return rpl_5;
-    }
+    txt_encriptado.textContent = encriptar(textarea);//imprimir en txt_encriptado el retorno de la funcion llamada
+    boton_copiar.classList.remove('btn_copiar')
 })
-btn_desencriptar.addEventListener('click', function(){// se le asigna el vento click al boton
+btn_desencriptar.addEventListener('click', function(){// se le asigna el evento click al boton
     const textarea = document.querySelector("textarea").value;//traemos el value del textarea
     txt_encriptado.classList.add('txt_encriptado');//clase para dar estilo donde aparece el texto encriptado
-    txt_encriptado.textContent = encriptar();//imprimir en txt_encriptado el retorno de la funcion llamada
-    //funcion para encriptar
-    function encriptar(){
-        let rpl_1 = textarea.replace(/enter/g, "e")
-        let rpl_2 = rpl_1.replace(/imes/g, "i")
-        let rpl_3 = rpl_2.replace(/ai/g, "a")        //remplazar encriptado por encriptado el texto que devuelve cada replace
-        let rpl_4 = rpl_3.replace(/ober/g, "o")
-        let rpl_5 = rpl_4.replace(/ufat/g, "u")
-        return rpl_5;
-    }
+    txt_encriptado.textContent = desencriptar(textarea);//imprimir en txt_encriptado el retorno de la funcion llamada
+    boton_copiar.classList.remove('btn_copiar')
+})
+//funcion para encriptar
+function encriptar(textarea){
+    let rpl_1 = textarea.replace(/e/g, "enter");
+    let rpl_2 = rpl_1.replace(/i/g, "imes");
+    let rpl_3 = rpl_2.replace(/a/g, "ai");        //remplazar letra por letra el texto que devuelve cada replace
+    let rpl_4 = rpl_3.replace(/o/g, "ober");
+    let rpl_5 = rpl_4.replace(/u/g, "ufat");
+    return rpl_5;
+}
+//funcion para desencriptar
+function desencriptar(textarea){
+    let rpl_1 = textarea.replace(/enter/g, "e");
+    let rpl_2 = rpl_1.replace(/imes/g, "i");
+    let rpl_3 = rpl_2.replace(/ai/g, "a");        //remplazar encriptado por encriptado el texto que devuelve cada replace
+    let rpl_4 = rpl_3.replace(/ober/g, "o");
+    let rpl_5 = rpl_4.replace(/ufat/g, "u");
+    return rpl_5;
+}
+boton_copiar.addEventListener('click', function(){
+    navigator.clipboard.writeText(txt_encriptado.textContent)
+    document.querySelector("textarea").focus();
 })
 // ----------------Funcion Anterior----------------
 // const textarea = document.querySelector("textarea").value.split("");// comvierte lo que hay en el textarea en array
